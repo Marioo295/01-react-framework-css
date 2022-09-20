355e7353dd68f3b046121a2780e37f6594f722e0
Marioo295 <mramosgaliana@gmail.com>
2022-09-20 13:55:29 +0200

    React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Bootstrap from './components/Bootstrap';
-import Bulma from './components/Bulma';
+/* import Bootstrap from './components/Bootstrap'; */
+/* import Bulma from './components/Bulma'; */
+import ReactBootstrap from './components/ReactBootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Fremework CSS con React</h1>
       {/* <Bootstrap></Bootstrap> */}
-      <Bulma></Bulma>
+      {/* <Bulma></Bulma> */}
+      <ReactBootstrap></ReactBootstrap>
     </div>
   );
 }
